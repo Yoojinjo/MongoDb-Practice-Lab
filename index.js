aa54023df8ea28d3655f9e9af74d41ca38eabc1c
Yoojinjo <eugenejo111@gmail.com>
2024-09-15 15:54:25 -0500

    add comments for index.js sections

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ import connectDb from "./db.js";
 const app = express();
 const PORT = 3000;
 
+//middleware
 app.use(express.json());
+
+//import routers
 import usersRouter from "./routes/users.js";
 import postsRouter from "./routes/posts.js";
 
+// use routers
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+
+// root route :)
 app.get("/", async (req, res) => {
 	try {
 		console.log("Hello");
@@ -16,12 +24,11 @@ app.get("/", async (req, res) => {
 		await res.send(results);
 	} catch (error) {
 		console.log(error);
+		res.status(500).json({ message: error.message });
 	}
 });
 
-app.use("/users", usersRouter);
-app.use("/posts", postsRouter);
-
+// start server and connect to MongoDB
 app.listen(PORT, () => {
 	console.log(`Listening on port: ${PORT}`);
 	connectDb();
